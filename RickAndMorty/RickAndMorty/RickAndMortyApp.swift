15479aa97682c93b349c63c550fdc2c2dffc2927
Manel Roca <manelrocarelats@gmail.com>
2024-07-26 23:07:10 +0200

    first commit

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Manel Roca on 26/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
